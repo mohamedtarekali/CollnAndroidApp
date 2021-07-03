98bfdf11bad95fe68d41a2e15402087109bea9bf
mohamedtarekali <68637tm@gmail.com>
2021-07-03 17:45:45 +0200

    first commit......

---
+++ COLNNANDROIDAPP/src/test/java/tests/AddLibraryTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import pages.DashBoardPage;
+import pages.LibraryPage;
+
+public class AddLibraryTest extends TestBase {
+	DashBoardPage dashBoardPageObject;
+	LibraryPage libraryPageObject;
+	 
+	@Test
+	public void userCanAddLibrary() {
+		dashBoardPageObject = new DashBoardPage(driver);
+		dashBoardPageObject.userOpenLibraryFromDashBoard();
+		libraryPageObject = new LibraryPage(driver);
+		libraryPageObject.userAddLibrary();
+	}
+
+}
